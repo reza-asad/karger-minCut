b6c94c9fd189da88d2c83929ca94fdd888818d49
reza-asad <rayza.asad@gmail.com>
2016-01-21 15:12:39 -0800

    graph structure cahnged

--- graph.py
+++ graph.py
@@ -8,9 +8,10 @@ class graph():
 	def __init__(self, data):
 		self.graph = data
 		self.edge_list = []
-		for key, value_list in self.graph.iteritems():
-			for value in value_list:
-				self.edge_list.append((key, value))
+		for key, values in self.graph.iteritems():
+			for value in values:
+				if key < value:
+					self.edge_list.append({key,value})
 	
 	# input: a node that currently does not exits in the 
 	# graph.
@@ -18,23 +19,33 @@ class graph():
 		if node in self.graph:
 			print 'The node already exists'
 		else:
-			self.graph[node] = []
+			self.graph[node] = set()
 	
 	# input: a pair of nodes from the list of existing
 	# nodes.
 	def add_edge(self,v,w):
 		if v in self.graph and w in self.graph:
-			self.graph[v].append(w)
-			self.graph[w].append(v)
+			self.graph[v].add(w)
+			self.graph[w].add(v)
 			# update edge_list
-			self.edge_list.append((v,w))
+			self.edge_list.append({v,w})
 		else:
 			print "At leat one of the nodes does not exist"
 
+	# input: an exisiting node
+	def drop_node(self, node):
+		if node not in self.graph:
+			print "The node does not exist"
+		else:
+			#for n in self.graph[node]:
+
+			del self.graph[node]
+
 ########################Main###############################
 # data 
-data = {1:[2,3], 2:[1,4], 3:[], 4:[]}
+data = {1:{2,3}, 2:{1,4}, 3: {1}, 4: {2}}
 g = graph(data)
 g.add_node(9)
 g.add_edge(3,9)
+#g.drop_node(9)
 print g.edge_list
