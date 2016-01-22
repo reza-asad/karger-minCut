6036b12466efd01738bc90d1ddb095f3cf77495e
reza-asad <rayza.asad@gmail.com>
2016-01-21 18:23:25 -0800

    base structure for the karger algorithm

--- karger_mincut.py
+++ karger_mincut.py
@@ -6,8 +6,19 @@ import random
 from graph import graph
 
 class mincut_problem(graph):
-	def compute_mincut(self):
+	# This is the edge contraction part of the algorithm
+	def contract_edge(self):
 		pass
+
+	# This computes the minimum cut of a graph
+	def compute_mincut(self):
+		num_vertices = len(data)
+		while num_vertices > 2:
+			random_edge = self.pick_random_edge()
+			self.contract_edge()
+			num_vertices -= 1
+		return len(self.edge_list)
+
 
 
 ######################## Main ###############################
@@ -15,3 +26,4 @@ class mincut_problem(graph):
 data = {1:{2,3}, 2:{1,4}, 3: {1}, 4: {2}}
 
 g = mincut_problem(data)
+print g.compute_mincut()
