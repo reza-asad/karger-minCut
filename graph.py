8257030f8b8e560156091e007ce2b60b99d286f1
reza-asad <rayza.asad@gmail.com>
2016-01-21 18:19:28 -0800

    changing the graph structure

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 # Reza Asad
 # Basic graph
 # Jan 28th, 2016
-#########################Algorithms######################
+######################### Algorithms ######################
 import random
 
 class graph():
@@ -10,8 +10,8 @@ class graph():
 		self.edge_list = []
 		for key, values in self.graph.iteritems():
 			for value in values:
-				if key < value:
-					self.edge_list.append({key,value})
+				if key < value :
+					self.edge_list.append((key,value))
 	
 	# input: a node that currently does not exits in the 
 	# graph.
@@ -19,16 +19,23 @@ class graph():
 		if node in self.graph:
 			print "{} already exists".format(node)
 		else:
-			self.graph[node] = set()
+			self.graph[node] = {}
 	
 	# input: a pair of nodes from the list of existing
 	# nodes.
 	def add_edge(self,v,w):
 		if v in self.graph and w in self.graph:
-			self.graph[v].add(w)
-			self.graph[w].add(v)
+			if w in self.graph[v]:
+				self.graph[v][w] += 1
+				self.graph[w][v] += 1
+			else:
+				self.graph[v][w] = 1
+				self.graph[w][v] = 1
 			# update edge_list
-			self.edge_list.append({v,w})
+			if v < w:
+				self.edge_list.append((v,w))
+			else:
+				self.edge_list.append((w,v))
 		else:
 			print "At leat one of the nodes in {} does not exist".format((v,w))
 
@@ -38,8 +45,11 @@ class graph():
 			print "node {} does not exist".format(node)
 		else:
 			for adjacent_node in self.graph[node]:
-				self.graph[adjacent_node].remove(node)
-				self.edge_list.remove({node,adjacent_node})
+				del self.graph[adjacent_node][node]
+				if node < adjacent_node:
+					self.edge_list = filter(lambda x: x != (node, adjacent_node), self.edge_list)
+				else:
+					self.edge_list = filter(lambda x: x != (adjacent_node, node), self.edge_list)
 			del self.graph[node]
 
 	# input: an existing edge
@@ -47,21 +57,21 @@ class graph():
 		if edge not in self.edge_list:
 			print "edge {} does not exist".format(edge)
 		else:
-			self.edge_list.remove(edge)
-			node1 = edge.pop()
-			node2 = edge.pop()
-			self.graph[node1].remove(node2)
-			self.graph[node2].remove(node1)
+			self.edge_list = filter(lambda x: x != edge, self.edge_list)
+			node1 = edge[0]
+			node2 = edge[1]
+			del self.graph[node1][node2]
+			del self.graph[node2][node1]
 
 	def pick_random_edge(self):
 		return random.choice(self.edge_list)
-########################Main###############################
+######################## Main ###############################
 # data 
-data = {1:{2,3}, 2:{1,4}, 3: {1}, 4: {2}}
+data = {1:{2:1,3:1}, 2:{1:1,4:2}, 3: {1:1}, 4: {2:2}}
 g = graph(data)
+g.add_edge(3,2)
+g.add_edge(3,2)
 g.add_node(9)
-g.add_edge(3,9)
-g.drop_node(1)
-g.add_node(3)
-#g.drop_edge({3,9})
-print g.pick_random_edge()
+#g.drop_node(2)
+g.drop_edge((2,3))
+print g.edge_list
